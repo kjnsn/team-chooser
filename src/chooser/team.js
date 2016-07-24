7a185c3fbfd19fbc0ac0d6f7b7377f17ca9badc2
Kaley Main <kaleypoole17@gmail.com>
2016-07-24 15:45:48 +1000

    Added logos and github link

--- src/chooser/team.js
+++ src/chooser/team.js
@@ -1,8 +1,25 @@
 import React, { PropTypes } from 'react';
 
+import instinct from './team-instinct-cutout.svg'
+import mystic from './team-mystic-cutout1-fixed.png'
+import valor from './team-valor-cutout1.svg'
+
+import './team.css'
+
+const image = (team) => {
+    switch (team) {
+        case 'red':
+            return valor
+        case 'blue':
+            return mystic
+        default:
+            return instinct
+    }
+}
+
 const Team = ({ team }) => (
-    <div>
-        <span>{team}</span>
+    <div className='team' >
+        <img src={image(team)} />
         <p>Refresh the page to select a new team</p>
     </div>
 )
